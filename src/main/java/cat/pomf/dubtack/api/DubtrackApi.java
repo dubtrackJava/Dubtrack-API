e7036f40057d68a040971b722d1d274140d1ce27
Discouragement <bacongucci@gmail.com>
2015-11-16 20:01:04 -0800

    account stuff

--- src/main/java/cat/pomf/dubtack/api/DubtrackApi.java
+++ src/main/java/cat/pomf/dubtack/api/DubtrackApi.java
@@ -1,11 +1,21 @@
 package cat.pomf.dubtack.api;
 
+import cat.pomf.dubtack.api.auth.DubtrackAccount;
+
 /**
  * @author Larry1123
  * @since 0.1.0
  */
 public class DubtrackApi {
+    private DubtrackAccount account;
 
+    public DubtrackApi(DubtrackAccount account){
+
+    }
+
+    public void login(){
+        // get dubtrack account, check if type is token or pw, login with it.
+    }
 
 
 }
--- src/main/java/cat/pomf/dubtack/api/auth/DubtrackAccount.java
+++ src/main/java/cat/pomf/dubtack/api/auth/DubtrackAccount.java
@@ -4,8 +4,8 @@ package cat.pomf.dubtack.api.auth;
  * Created by root on 11/16/2015.
  */
 public class DubtrackAccount {
-    private String username, password;
-
+    private String username, password, token;
+    private AccountType accountType;
     public String getUsername() {
         return username;
     }
@@ -18,6 +18,17 @@ public class DubtrackAccount {
     public DubtrackAccount(String username, String password) {
         this.username = username;
         this.password = password;
+        this.accountType = AccountType.LOGIN;
+    }
+
+    public DubtrackAccount(String token){
+        this.token = token;
+        this.accountType = AccountType.TOKEN;
+    }
+
+    private enum AccountType{
+        LOGIN,
+        TOKEN;
     }
 
 
